8fe8aa59ead024c9489aadba23d88106b268eaab
Akarys Shorabek <akarys.shorabek@akvelon.com>
2025-02-18 16:37:57 +0500

    asd

--- sdks/python/container/run_validatescontainer.sh
+++ sdks/python/container/run_validatescontainer.sh
@@ -122,7 +122,7 @@ cd sdks/python
 SDK_LOCATION=$2
 
 echo ">>> RUNNING DATAFLOW RUNNER VALIDATESCONTAINER TEST"
-pytest -o log_cli=True -o log_level=debug -o junit_suite_name=$IMAGE_NAME \
+pytest -o log_cli=True -o log_level=Debug -o junit_suite_name=$IMAGE_NAME \
   -m=it_validatescontainer \
   --numprocesses=1 \
   --timeout=3600 \
