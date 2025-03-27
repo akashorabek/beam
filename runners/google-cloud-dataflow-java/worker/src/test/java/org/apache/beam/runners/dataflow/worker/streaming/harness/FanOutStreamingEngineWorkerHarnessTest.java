0f8f0c47d0c1ae702655f1fb76c937b9b8686ad7
Akarys Shorabek <akarys.shorabek@akvelon.com>
2025-03-27 17:07:20 +0500

    changes

--- runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/streaming/harness/FanOutStreamingEngineWorkerHarnessTest.java
+++ runners/google-cloud-dataflow-java/worker/src/test/java/org/apache/beam/runners/dataflow/worker/streaming/harness/FanOutStreamingEngineWorkerHarnessTest.java
@@ -307,6 +307,7 @@ public class FanOutStreamingEngineWorkerHarnessTest {
   public void testOnNewWorkerMetadata_redistributesBudget() throws InterruptedException {
     String workerToken = "workerToken1";
     String workerToken2 = "workerToken2";
+    assertEquals(1, 2);
 
     WorkerMetadataResponse firstWorkerMetadata =
         WorkerMetadataResponse.newBuilder()
